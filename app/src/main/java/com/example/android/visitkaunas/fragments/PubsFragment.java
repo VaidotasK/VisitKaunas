889a188a17b944c827077605fca0211e5f62a321
Vaidotas <vaidotas.karoblis@gmail.com>
2018-07-15 04:10:16 +0300

    Added full array list for Pubs. Set adapter on listview and set OnItemClickListener on list item. Created new object of InformationFullExtraProvider class and used method provideExtra...to save currently pressed place's information and send it to PlaceInformationFullActivity.

--- app/src/main/java/com/example/android/visitkaunas/fragments/PubsFragment.java
+++ app/src/main/java/com/example/android/visitkaunas/fragments/PubsFragment.java
@@ -6,8 +6,15 @@ import android.support.v4.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
+import com.example.android.visitkaunas.InformationFullExtraProvider;
+import com.example.android.visitkaunas.Place;
+import com.example.android.visitkaunas.PlaceAdapter;
 import com.example.android.visitkaunas.R;
+
+import java.util.ArrayList;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -23,8 +30,43 @@ public class PubsFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.place_list, container, false);
+
+        View rootView = inflater.inflate(R.layout.place_list, container, false);
+
+        final ArrayList<Place> places = new ArrayList<Place>();
+        places.add(new Place(getString(R.string.pub_name_1), getString(R.string.pub_address_1), "", getString(R.string.pub_description_1)));
+        places.add(new Place(getString(R.string.pub_name_2), getString(R.string.pub_address_2), "", getString(R.string.pub_description_2)));
+        places.add(new Place(getString(R.string.pub_name_3), getString(R.string.pub_address_3), "", getString(R.string.pub_description_3)));
+        places.add(new Place(getString(R.string.pub_name_4), getString(R.string.pub_address_4), "", getString(R.string.pub_description_4)));
+        places.add(new Place(getString(R.string.pub_name_5), getString(R.string.pub_address_5), "", getString(R.string.pub_description_5)));
+        places.add(new Place(getString(R.string.pub_name_6), getString(R.string.pub_address_6), "", getString(R.string.pub_description_6)));
+        places.add(new Place(getString(R.string.pub_name_7), getString(R.string.pub_address_7), "", getString(R.string.pub_description_7)));
+        places.add(new Place(getString(R.string.pub_name_8), getString(R.string.pub_address_8), "", getString(R.string.pub_description_8)));
+        places.add(new Place(getString(R.string.pub_name_9), getString(R.string.pub_address_9), "", getString(R.string.pub_description_9)));
+        places.add(new Place(getString(R.string.pub_name_10), getString(R.string.pub_address_10), "", getString(R.string.pub_description_10)));
+
+
+        PlaceAdapter adapter = new PlaceAdapter(getActivity(), places);
+
+        ListView listView = rootView.findViewById(R.id.list);
+
+        listView.setAdapter(adapter);
+
+        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long l) {
+
+                Place place = places.get(position);
+
+                InformationFullExtraProvider informationFullExtraProvider =
+                        new InformationFullExtraProvider();
+                informationFullExtraProvider.provideExtraForInformationFullActivity(getContext(),
+                        place.getPlaceName(), place.getPlaceImageResourceId(),
+                        place.getPlaceAddress(), place.getPlacePrice(), place.getPlaceDescription());
+
+            }
+        });
+        return rootView;
     }
 
 }
